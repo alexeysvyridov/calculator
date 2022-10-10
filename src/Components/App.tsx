72a4f254568eb05a2918fe23ca1451d9a811a866
Aleksey.Svyrydov@BankLeumi.co.il <Aleksey.Svyrydov@BankLeumi.co.il>
2022-10-10 19:42:31 +0300

    added all functionallity

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Wrapper } from './Wrapper';
 import { Screen } from './Screen';
@@ -7,16 +7,148 @@ import { Button } from './Button';
 
 const btnValues = [
   ["C", "+-", "%", "/"],
-  [7,8,9, "X"],
+  [7, 8, 9, "X"],
   [4, 5, 6, "-"],
-  [1 , 2 , 3, "+"],
+  [1, 2, 3, "+"],
   [0, ".", "="],
-]
+];
+
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
 function App():JSX.Element {
+  const [calc, setCalc] = useState<{
+    sign: string,
+    num: number,
+    res: number,
+  }>({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler = (value) => {
+    if (removeSpaces(calc.num).length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
+
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
+      sign: "",
+    });
+  };
+
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+  
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    });
+  };
+
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) => {
+        if (sign === '+') return a + b;
+        if (sign === '-') return a - b;
+        if (sign === 'X') return a * b;
+        return a / b;
+      }
+
+      setCalc({
+        ...calc,
+        res: 
+          calc.num === "0" && calc.sign === "/"
+          ? "Can't divide with 0"
+          : toLocaleString(
+              math(
+                Number(removeSpaces(calc.res)),
+                Number(removeSpaces(calc.num)),
+                calc.sign
+              )
+          ),
+        sign: "",
+        num: 0,
+      })
+    }
+  };
+
+  const signClickHandler = (value) => {
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    })
+  };
+  const commaClickHandler = (value) => {
+    setCalc({
+      ...calc,
+      num: !calc.num.includes(".") ? calc.num + value : calc.num,
+    })
+  };
+  const handleActionClick = (btn, e) => {
+    e.preventDefault()
+    const value = e.target.innerHTML;
+
+    switch (btn) {
+      case "C":
+        resetClickHandler()
+        break;
+      case "+-": 
+        invertClickHandler()
+        break;
+      case "%": 
+        percentClickHandler()
+        break;
+      case "=": 
+        equalsClickHandler()
+        break;
+      case "/": 
+      case "X": 
+      case "-": 
+      case "+": 
+        signClickHandler(value)
+        break;
+      case ".": 
+        commaClickHandler(value)
+        break;
+      default:
+        numClickHandler(value)
+        break;
+    }
+  };
+
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
         {btnValues.flat().map((btn, i) => {
           return (
@@ -24,9 +156,7 @@ function App():JSX.Element {
             key={i}
             className={btn === "=" ? "equals" : ""}
             value={btn}
-            onClick={() => {
-              console.log(`click ${btn}`)
-            }}
+            onClick={(e) => handleActionClick(btn, e)}
           />
           )
         })}
